493f54aa9a71c8805a0bb18a5e3c6026d18ac11d
MaiHuyTech <maihuytech@gmail.com>
2022-01-05 11:21:34 +0700

    init

---
+++ express.js
@@ -0,0 +1,16 @@
+const { response } = require("express");
+const { request } = require("http");    
+const express = require("express");
+const app = express();
+app.use(express.json({extended: false}));
+app.use(express.static('./css'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.listen(3000,()=>{
+    console.log('server is start at port 3000');
+})
+app.get('/', (req,res) =>{      
+    return res.render('index');
+    
+});
+
